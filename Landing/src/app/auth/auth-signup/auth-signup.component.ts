e1bd80429c04d583b8a3f73457231573f2ea4b47
Piotr Świerczyński <piotrswierczynski50@gmail.com>
2022-02-19 12:15:57 +0100

    pobieranie info z rejestracji

--- Landing/src/app/auth/auth-signup/auth-signup.component.ts
+++ Landing/src/app/auth/auth-signup/auth-signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import {ChangeDetectorRef} from '@angular/core';
 
 @Component({
   selector: 'app-auth-signup',
@@ -11,9 +14,27 @@ import { Component, OnInit } from '@angular/core';
  */
 export class AuthSignupComponent implements OnInit {
 
-  constructor() { }
+  newUser: Array<SignUp>;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  addNewUser(form: any) {
+    const new_user = new SignUp();
+    new_user.first_name = form.value.first_name;
+    new_user.last_name = form.value.last_name;
+    new_user.email = form.value.email;
+    new_user.password = form.value.password;
+    this.newUser.push(new_user);
+  }
+}
+
+class SignUp{
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
 }
