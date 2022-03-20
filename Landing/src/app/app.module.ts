a284d08671cb0a179e4dbafcc9cfda8f04d5f689
Piotr Świerczyński <piotrswierczynski50@gmail.com>
2022-03-20 16:04:57 +0100

    czesciowa walidacja przy rejestracji

--- Landing/src/app/app.module.ts
+++ Landing/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { LightboxModule } from 'ngx-lightbox';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -83,6 +83,7 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
     NgxMasonryModule,
     LightboxModule,
     HttpClientModule,
+    ReactiveFormsModule,
 
   ],
   exports: [
--- Landing/src/app/auth/auth-signup/auth-signup.component.ts
+++ Landing/src/app/auth/auth-signup/auth-signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgForm } from '@angular/forms';
+import {FormControl, FormGroup, FormsModule} from '@angular/forms';
+import { NgForm, Validators } from '@angular/forms';
 import {ChangeDetectorRef} from '@angular/core';
 import { UserService } from '../../shared/services/user.service';
 import {HttpErrorResponse} from "@angular/common/http";
@@ -18,25 +18,70 @@ import {Router} from "@angular/router";
  */
 export class AuthSignupComponent implements OnInit {
 
+    information_to_user = '';
+    isChecked : boolean;
+    userEmail='';
+
     constructor(private userService: UserService){
     }
 
     ngOnInit(): void {
     }
+    userEmails = new FormGroup({
+        primaryEmail: new FormControl('',[
+            Validators.required,
+            Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
+    });
 
+    get email(){
+        return this.userEmails.get('primaryEmail');
+    }
     addNewUser(form: any) {
-        this.userService.registerUser(form.value.firstName, form.value.lastName, form.value.email, form.value.password).subscribe(
-            (response: any) => {
-              console.log(response);
-
-              // this.getEmployees();
-              form.reset();
-            },
-            (error: HttpErrorResponse) => {
-              alert(error.message);
-              form.reset();
+        if(form.value.firstName != ''){
+            if(form.value.lastName != ''){
+                if(form.value.email=''){
+                    // if(form.value.password == form.value.repeat_password) {
+                    //     if(this.isChecked == true){
+                    //         console.log("zaznaczono");
+                    //     }
+                    //     else{
+                    //         this.information_to_user="Nie zaakcpetowano regulaminu";
+                    //         form.reset();
+                    //     }
+                    // }
+                    // else{
+                    //     this.information_to_user="Hasła są różne";
+                    //     form.reset();
+                    // }
+                }
+                else{
+                    this.information_to_user="Nie podano emailu";
+                    form.reset();
+                }
             }
-        );
+            else{
+                this.information_to_user="Nie wypelniono wszystkich wymaganych pól";
+                form.reset();
+            }
+
+        }
+        else{
+            this.information_to_user="Nie wypelniono wszystkich wymaganych pól";
+            form.reset();
+        }
+
+        // this.userService.registerUser(form.value.firstName, form.value.lastName, form.value.email, form.value.password).subscribe(
+        //     (response: any) => {
+        //       console.log(response);
+        //
+        //       // this.getEmployees();
+        //       form.reset();
+        //     },
+        //     (error: HttpErrorResponse) => {
+        //       alert(error.message);
+        //       form.reset();
+        //     }
+        // );
 
     }
 }
