dc4760287e12450fbaa82ca666f6e2c8e7ca551c
Piotr Świerczyński <piotrswierczynski50@gmail.com>
2022-03-20 11:20:06 +0100

    dodano find-patient

---
+++ Landing/src/app/core/components/find-patient/find-patient.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FindPatientComponent } from './find-patient.component';
+
+describe('FindPatientComponent', () => {
+    let component: FindPatientComponent;
+    let fixture: ComponentFixture<FindPatientComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ FindPatientComponent ]
+        })
+            .compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FindPatientComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ Landing/src/app/core/components/find-patient/find-patient.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import {AccountOutput, DiseasesOutput, AllergiesOutput, ContactsOutput, MedicationsOutput} from "../../../shared/models/account.model";
+import {AccountService} from "../../../shared/services/account.service";
+import {UserService} from "../../../shared/services/user.service";
+import {Router} from "@angular/router";
+
+@Component({
+    selector: 'app-find-patient',
+    templateUrl: './find-patient.component.html',
+    styleUrls: ['./find-patient.component.css']
+})
+
+/**
+ * Account Profile Component
+ */
+export class FindPatientComponent implements OnInit {
+
+    /**
+     * nav light class add
+     */
+    navClass = 'nav-light';
+    name = '';
+    lastName = '';
+    email = '';
+    code = '';
+    illnessList: Array<DiseasesOutput>;
+    allegriesList: Array<AllergiesOutput>;
+    contactList: Array<ContactsOutput>;
+    medicationsList: Array<MedicationsOutput>;
+
+    constructor(private accountService: AccountService,
+                private userService: UserService,
+                private router: Router) {
+    }
+
+    ngOnInit(): void {
+        this.userService.getFromRegistration().subscribe(
+            (information: AccountOutput) => {
+
+                this.name = information.name;
+                this.lastName = information.lastName;
+                this.email = information.email;
+                this.code = information.code;
+
+                this.accountService.getChronicDiseases(information.code).subscribe(
+                    (data: Array<DiseasesOutput>) => {
+                        this.illnessList = data;
+                    },
+                    () => {
+                    }
+                );
+                this.accountService.getAllergies(information.code).subscribe(
+                    (data: Array<AllergiesOutput>) => {
+                        this.allegriesList = data;
+                    },
+                    () => {
+                    }
+                );
+                this.accountService.getContacts(information.code).subscribe(
+                    (data: Array<ContactsOutput>) => {
+                        this.contactList = data;
+                    },
+                    () => {
+                    }
+                );
+                this.accountService.getMedications(information.code).subscribe(
+                    (data: Array<MedicationsOutput>) => {
+                        this.medicationsList = data;
+                    },
+                    () => {
+                    }
+                );
+            },
+            () => {
+            }
+        );
+
+
+    }
+    logout(){
+        this.userService.removeLocalUser();
+    }
+}
