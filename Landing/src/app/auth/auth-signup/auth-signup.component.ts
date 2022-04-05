c728b9f384defca14b6bb3ab7fd24d8165a64cc1
Piotr Świerczyński <piotrswierczynski50@gmail.com>
2022-04-05 20:02:15 +0200

    nowa walidacja rejestracji

--- Landing/src/app/auth/auth-signup/auth-signup.component.ts
+++ Landing/src/app/auth/auth-signup/auth-signup.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, FormsModule} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, FormsModule} from '@angular/forms';
 import { NgForm, Validators } from '@angular/forms';
 import {ChangeDetectorRef} from '@angular/core';
 import { UserService } from '../../shared/services/user.service';
 import {HttpErrorResponse} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {PatientService} from "../../shared/services/patient.service";
+import {MustMatch} from "../../shared/match_validator/must_match.validator";
+
 
 
 @Component({
@@ -20,92 +22,62 @@ import {PatientService} from "../../shared/services/patient.service";
 export class AuthSignupComponent implements OnInit {
 
     information_to_user = '';
-    isChecked : boolean;
     web = true;
 
+    registerForm: FormGroup;
+    submitted = false;
 
     constructor(private userService: UserService,
                 private patientService: PatientService,
-                private router: Router){
+                private router: Router,
+                private formBuilder: FormBuilder){
     }
 
     ngOnInit(): void {
+        this.registerForm = this.formBuilder.group({
+            firstName: new FormControl('', [Validators.required]),
+            lastName: new FormControl('', [Validators.required]),
+            email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+            password: new FormControl('', [Validators.required, Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,16}$")]),
+            confirmPassword: new FormControl('', [Validators.required]),
+            rules: new FormControl(false,[Validators.requiredTrue]),
+        }, {
+            validator: MustMatch('password', 'confirmPassword')
+        });
         if (window.innerWidth <= 768) { // 768px portrait
             this.web = false;
         }
     }
-    userEmails = new FormGroup({
-        primaryEmail: new FormControl('',[
-            Validators.required,
-            Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
-    });
-
-    get email(){
-        return this.userEmails.get('primaryEmail');
-    }
+    get f() { return this.registerForm.controls; }
 
     addNewUser(form: any) {
-        if(form.value.firstName != ''){
-            if(form.value.lastName != '') {
-                if (this.userEmails.get('primaryEmail').value != '') {
-                    this.patientService.existsEmail(this.userEmails.get('primaryEmail').value).subscribe(
-                        (data: any) => {
-                            if (data.exists == false) {
-                                if(form.value.password != ''){
-                                    if(form.value.password == form.value.repeat_password) {
-                                        if(this.isChecked == true){
-                                            this.userService.registerUser(form.value.firstName, form.value.lastName, this.userEmails.get('primaryEmail').value , form.value.password).subscribe(
-                                                (response: any) => {
-                                                    this.router.navigate(['/confirm-mail', form.value.firstName]);
-                                                },
-                                                (error: HttpErrorResponse) => {
-                                                    form.reset();
-                                                }
-                                            );
-                                        }
-                                        else{
-                                            this.information_to_user = 'Nie zaakcpetowano regulaminu';
-                                            form.reset();
-                                        }
-                                    }
-                                    else{
-                                        this.information_to_user = 'Hasła są różne';
-                                        form.reset();
-                                    }
-                                }
-                                else{
-                                    this.information_to_user = 'Nie wpisano hasła';
-                                    form.reset();
-                                }
-                            }
-                            else {
-                                this.information_to_user = 'Podany email jest już użyty';
-                                form.reset();
-                            }
-                        },
-                        () => {
-                        }
-                    );
+        this.submitted = true;
 
 
-                }
-                else {
-                    this.information_to_user = 'Nie podano emailu';
-                    form.reset();
-                }
-            }
-            else{
-                this.information_to_user = 'Nie wypelniono wszystkich wymaganych pól';
-                form.reset();
-            }
-
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
         }
         else{
-            this.information_to_user = 'Nie wypelniono wszystkich wymaganych pól';
-            form.reset();
+            this.patientService.existsEmail(form.value.email).subscribe(
+            (data: any) => {
+                if (data.exists == false) {
+                    this.userService.registerUser(form.value.firstName, form.value.lastName, form.value.email, form.value.password).subscribe(
+                        (response: any) => {
+                            this.router.navigate(['/confirm-mail', form.value.firstName]);
+                        },
+                        (error: HttpErrorResponse) => {
+                            form.reset();
+                        }
+                    );
+                }
+                else {
+                    this.information_to_user = 'Podany email jest już użyty';
+                }
+            },
+            () => {
+            }
+        );
         }
-
-
-
     }
 }
